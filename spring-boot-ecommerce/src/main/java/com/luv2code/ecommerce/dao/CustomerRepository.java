2f034c66af519dbb31eb2e1a1e346cf44dcdd86b
xAnt01x <antluisi2001@gmail.com>
2024-01-10 15:45:39 +0100

    Update

---
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    // SELECT * FROM Customer c WHERE c.email = theEmail
+    Customer findByEmail(String theEmail);
+}
---
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // SELECT * FROM Product WHERE category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+
+    // SELECT * FROM Product p WHERE p.name LIKE CONCAT('%', :name ,'%')
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+}
